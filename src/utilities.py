db22abfaaf42d41ca80eb24c8c60635e03d41e6b
Seed Snake <119754405+SeedSnake@users.noreply.github.com>
2024-04-25 11:53:18 +0200

    Update 1.3.2

--- src/utilities.py
+++ src/utilities.py
@@ -5,17 +5,19 @@ def number_to_emoji(number):
     }
     return ''.join(digit_to_emoji[digit] for digit in str(number))
 
-def evaluate_sentiment(coinbase_tracker, wallet_tracker):
-    # Assuming coinbase_tracker and wallet_tracker are already created and passed to this function
+def evaluate_sentiment(coinbase_tracker, wallet_tracker, binance_tracker, cryptodotcom_tracker):
+    # Assuming coinbase_tracker and other apps are already created and passed to this function
 
     # Fetch the current ranks from tracker instances
     coinbase_current_rank, _ = coinbase_tracker.get_previous_rank()
     wallet_current_rank, _ = wallet_tracker.get_previous_rank()
+    binance_current_rank, _ = binance_tracker.get_previous_rank()
+    cryptodotcom_current_rank, _ = cryptodotcom_tracker.get_previous_rank()
 
-    print(f"Debug: Coinbase Rank: {coinbase_current_rank}, Wallet Rank: {wallet_current_rank}")
+    print(f"Debug: Coinbase Rank: {coinbase_current_rank}, Wallet Rank: {wallet_current_rank}, Binance Rank: {binance_current_rank}, Crypto.com Rank: {cryptodotcom_current_rank}")
 
     # Check if either rank is None
-    if None in (coinbase_current_rank, wallet_current_rank):
+    if None in (coinbase_current_rank, wallet_current_rank, binance_current_rank, cryptodotcom_current_rank):
         print("Debug: One or both ranks are None.")
         return "No data available for sentiment analysis."
     
@@ -23,25 +25,43 @@ def evaluate_sentiment(coinbase_tracker, wallet_tracker):
         # Ensure ranks are integers for calculation
         coinbase_current_rank = int(coinbase_current_rank)
         wallet_current_rank = int(wallet_current_rank)
+        binance_current_rank = int(binance_current_rank)
+        cryptodotcom_current_rank = int(cryptodotcom_current_rank)
         # Calculate weighted average
-        weighted_average_rank = (2 * coinbase_current_rank + wallet_current_rank) / 3
+        weighted_average_rank = (5 * (coinbase_current_rank + cryptodotcom_current_rank) + 2.5 * binance_current_rank + wallet_current_rank) / 13.5
 
         # Evaluate sentiment based on the weighted average
-        if weighted_average_rank <= 5:
-            return "``🟢 🟢 🟢 Extreme Greed!``"
-        elif weighted_average_rank <= 15:
-            return "``🟢 🟢 Greed!``"
+        if weighted_average_rank <= 10:
+            sentiment = "Extreme Greed"
+        elif weighted_average_rank <= 20:
+            sentiment = "Greed"
         elif weighted_average_rank <= 25:
-            return "``🟢 Belief``"
+            sentiment = "Optimism"
+        elif weighted_average_rank <= 30:
+            sentiment = "Doubt"
         elif weighted_average_rank <= 35:
-            return "``🟡 Doubt``"
+            sentiment = "Anxiety"
         elif weighted_average_rank <= 50:
-            return "``🟡 🟠 Anxiety``"
-        elif weighted_average_rank <= 60:
-            return "``🟠 🔴 Fear!``"
+            sentiment = "Fear"
         else:
-            return "``🔴 🔴 🔴Capitulation!``"
+            sentiment = "Capitulation"
 
+        # Add a match sentiment-image
+        sentiment_images = {
+            "Extreme Greed": "extreme_greed.png",
+            "Greed": "greed.png",
+            "Optimism": "optimism.png",
+            "Doubt": "doubt.png",
+            "Anxiety": "anxiety.png",
+            "Fear": "fear.png",
+            "Capitulation": "capitulation.png"
+        }
+
+        # Choose images based on current sentiment
+        image_file = sentiment_images.get(sentiment)
+
+        return sentiment, image_file
+    
     except TypeError as e:
         print(f"Error in evaluate_sentiment: {e}")
         return "Error processing rank values."
@@ -49,4 +69,3 @@ def evaluate_sentiment(coinbase_tracker, wallet_tracker):
     except ValueError as e:
         print(f"Error converting rank values to integers: {e}")
         return "Error processing rank values."
-
