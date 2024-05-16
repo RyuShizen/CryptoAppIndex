b8eb2dfd0a67a667d57f5d22c2f111af03ffa4c1
SeedSnake <alpha.dawg.ez@gmail.com>
2024-05-16 15:44:34 +0200

    update 1.6.2.1

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,11 @@
+#                     GNU GENERAL PUBLIC LICENSE
+#                        Version 3, 29 June 2007
+#                     SeedSnake | CryptoAppIndex
+
+#  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+#  Everyone is permitted to copy and distribute verbatim copies
+#  of this license document, but changing it is not allowed.
+
 from discord.ext import commands
 from discord import Intents
 import discord
